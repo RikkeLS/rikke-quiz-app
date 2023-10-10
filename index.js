65065024428f03d94b8ff314c0dd9f41ce845b04
Rikke Saust <rlsaust@gmail.com>
2023-10-10 12:57:27 +0200

    test other method to change bookmark icon when clicking

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ console.clear()
 const bookmark = document.querySelector('[data-js="card__bookmark"]');
 const answerButton = document.querySelector('[data-js="card__button-answer"]');
 const answerToHide = document.querySelector('[data-js="card__answer"]');
-
+const questionForm = document.querySelector('[data-js="form-question"]');
 
 // click event for bookmark with arrow function:
 bookmark.addEventListener('click', (event)=> {
-    const currentBookmark = bookmark.src;
+    const currentBookmark = bookmark.src; // gives a longer path, https://'more'
+    // cutting the string into just the filename
     currentImageFilename=currentBookmark.split("/")[currentBookmark.split("/").length-1]
+    // gives a path which does not need to be cut
+    // const currentBookmark = bookmark.getAttribute('src') 
 
     if (currentImageFilename === "bookmark_filled.png") {
         bookmark.src = "./resources/bookmark.png"  
@@ -30,3 +33,8 @@ answerButton.addEventListener('click', (event) => {
         answerButton.textContent = 'Hide Answer'
     }
 });
+
+// get data from the form to create a card
+// questionForm.addEventListener('submit', (event) => {
+    
+// });
