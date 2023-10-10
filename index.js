7f33824d974cfd7cd4dd9a1a14a177f1bed59f0a
Rikke Saust <rlsaust@gmail.com>
2023-10-10 11:03:28 +0200

    function for answer button, toggling hidden attribute and change text

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 console.clear()
 
 // query selection:
-
 const bookmark = document.querySelector('[data-js="card__bookmark"]');
+const answerButton = document.querySelector('[data-js="card__button-answer"]');
+const answerToHide = document.querySelector('[data-js="card__answer"]');
 
 
 // click event for bookmark with arrow function:
 bookmark.addEventListener('click', (event)=> {
-    console.log('bookmark click');
-
     const currentBookmark = bookmark.src;
     currentImageFilename=currentBookmark.split("/")[currentBookmark.split("/").length-1]
 
-    console.log(currentImageFilename);
     if (currentImageFilename === "bookmark_filled.png") {
         bookmark.src = "./resources/bookmark.png"  
     }
     if (currentImageFilename === "bookmark.png") {
         bookmark.src = "./resources/bookmark_filled.png"  
+    }
+});
+
+// click event for answer button, for hiding answer and change button text:
+answerButton.addEventListener('click', (event) => {
+    answerToHide.toggleAttribute('hidden')
+    
+    if (answerToHide.hidden==true) {
+        answerButton.textContent = 'Show Answer'
+    }
+    if (answerToHide.hidden==false) {
+        answerButton.textContent = 'Hide Answer'
     }
 });
