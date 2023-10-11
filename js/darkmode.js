6a929f9b48eb8f573f3e09fb2328be203bbdad68
Rikke Saust <rlsaust@gmail.com>
2023-10-11 10:43:15 +0200

    simple darkmode toggle

---
+++ js/darkmode.js
@@ -0,0 +1,27 @@
+console.clear()
+
+const darkModeButton = document.querySelector('[data-js="settings__dark-mode-section__box--button"]');
+
+let Mode = 0;
+darkModeButton.addEventListener('click', (event)=> {
+    
+    
+    if (Mode == 0) {
+        document.documentElement.style.setProperty('--color-std-text', 'white');
+        document.documentElement.style.setProperty('--color-std-background','black');
+        document.documentElement.style.setProperty('--color-header-background','purple');
+        document.documentElement.style.setProperty('--color-footer-background','purple');
+        document.documentElement.style.setProperty('--color-current-page','rgb(231, 171, 231)');
+        Mode = 1;
+    
+    } else {
+        document.documentElement.style.setProperty('--color-std-text', 'blue');
+        document.documentElement.style.setProperty('--color-std-background','white');
+        document.documentElement.style.setProperty('--color-header-background','green');
+        document.documentElement.style.setProperty('--color-footer-background','green');
+        document.documentElement.style.setProperty('--color-current-page','darkgreen');
+        Mode = 0;
+    }
+
+        
+})
